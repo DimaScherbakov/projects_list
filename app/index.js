b4e4644c06d975b71acc35c928a12b4eeb3c6b66
dimaScherbakov <dima.r.scherbakov@gmail.com>
2018-10-04 22:25:15 +0300

    add some request fixes

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var session = require("express-session");
 var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
-var cors = require('cors');
+//var cors = require('cors');
 var getCurUser = require("./login").getCurUser;
 var getUserCatByName =  require("./login").getUserCatByName;
 var getProjectsList = require("./list");
@@ -13,25 +13,25 @@ app.use(bodyParser.json());
 // var sessionHandler = require("./js/session_handler");
 // var store = sessionHandler.createStore();
 // app.use(cookieParser());
-//app.use(function (req, res, next) {
-//	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
-//	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//	next();
-//});
-var originsWhitelist = [
-  'http://localhost:4200', 'http://www.myproductionurl.com'
-];
-var corsOptions = {
- origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
-  },
-  credentials:true
-}
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	next();
+});
+//var originsWhitelist = [
+//  'http:\/\/localhost:4200', 'http://www.myproductionurl.com'
+//];
+//var corsOptions = {
+// origin: function(origin, callback){
+//        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+//        callback(null, isWhitelisted);
+//  },
+//  credentials:true
+//}
 
 //here is the magic
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 var sessionStore={userName:undefined,sessionId:undefined,curSessionId:undefined};
 app.use(session({

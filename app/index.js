c97bd99325c60c5af4e5335eb6ba56766cac83eb
dimaScherbakov <dima.r.scherbakov@gmail.com>
2018-10-08 13:50:13 +0300

    removed unuseful

--- app/index.js
+++ app/index.js
@@ -9,30 +9,26 @@ var getCurUser = require("./login").getCurUser;
 var getUserCatByName =  require("./login").getUserCatByName;
 var getProjectsList = require("./list");
 app.use(bodyParser.json());
-//app.use(express.bodyParser());
-// var sessionHandler = require("./js/session_handler");
-// var store = sessionHandler.createStore();
-//app.use(cookieParser());
 
-//app.use(function (req, res, next) {
-//	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
-//	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//	next();
-//});
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://127.0.0.1:4200");
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	next();
+});
 
-var originsWhitelist = [
-  'http:\/\/localhost:4200', 'http://www.myproductionurl.com'
-];
-var corsOptions = {
- origin: function(origin, callback){
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        callback(null, isWhitelisted);
-  },
-  credentials:true
-}
+//var originsWhitelist = [
+//  'http:\/\/localhost:4200', 'http://www.myproductionurl.com'
+//];
+//var corsOptions = {
+// origin: function(origin, callback){
+//        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+//        callback(null, isWhitelisted);
+//  },
+//  credentials:true
+//}
 //here is the magic
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 var sessionStore={userName:undefined,sessionId:undefined};
 
@@ -44,44 +40,19 @@ app.use(session({
 //	cookie: { secure: true, maxAge: 60000 }
 }));
 
-// console.log(store)
-//app.get('/login', function(req, res){
-//    res.sendFile(__dirname + '/login/login-form.html');
-//})
-
 app.post('/login',function(req,res){
 		console.log("/login has been loaded");
 		curUser = getCurUser(req.body.name,req.body.pass);
 		if(curUser){sessionStore.userName=curUser.name;
-			//sessionStore.sessionId=req.session.id;
-			//req.session.uName=curUser.name;
-			console.log(curUser);
-			//var sessData = req.session;
-			//console.log(sessData);
-			//sessData.store = curUser.name;
+			session.uName=curUser.name;
 			}
-		else {sessionStore.userName="guest"};
-		//console.log(req.session.store);
-		//req.session.usercat = curUser.cat||"guest";
-		//console.log(req.session.id);
-		session.uName=curUser.name;
-		console.log(session);
+		else {sessionStore.userName=""; session.uName="";}
 		res.send(JSON.stringify(curUser));
-		//var list = getProjectsList(curUser.cat);
-		//res.send(JSON.stringify(list));
 })
 
 app.get('/list', function(req,res){
 	var cat = "guest";
-	console.log("/list has been loaded");
-	console.log(session);
-	//console.log(sessionStore.sessionId+" "+req.session.id);
-	//console.log(req.session.uName);	
-	// console.log(req.session.id,req.session.usercat);
-	//console.log("sessionStore: "+sessionStore.userName+" "+sessionStore.sessionId+" "+sessionStore.curSessionId);
-	if(sessionStore.userName==session.uName){cat = getUserCatByName(sessionStore.userName);console.log("CAT") }
-	//var cat = req.session.usercat || "guest";
-	console.log("cat: "+cat);
+	if(sessionStore.userName==session.uName){cat = getUserCatByName(sessionStore.userName); }
 	var list = getProjectsList(cat);
 	res.send(JSON.stringify(list));
 })
--- app/login/index.js
+++ app/login/index.js
@@ -12,12 +12,11 @@ function getCurUser(name,pass) {
 }
 
 function getUserCatByName(uName){
-console.log("getUSerCatByName works with arg: "+uName);
 var answer = users.filter(function(item){
 return item.name == uName;
 });
 console.log(answer);
-return answer[0].cat;
+return answer[0]? answer[0].cat : "guest";
 }
 module.exports.getCurUser = getCurUser;
 module.exports.getUserCatByName = getUserCatByName;
